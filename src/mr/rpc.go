bb8f3d466fb7e017e9925eae2b0bc6e1a86e4310
root <ricemaster.likai@gmail.com>
2020-05-31 10:50:46 +0800

    mr v0.1

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,24 +6,62 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
-//
-// example to show how to declare the arguments
+//ExampleArgs example to show how to declare the arguments
 // and reply for an RPC.
 //
-
 type ExampleArgs struct {
 	X int
 }
 
+//ExampleReply xxx
 type ExampleReply struct {
 	Y int
 }
 
 // Add your RPC definitions here.
 
+//MapArgs xxx
+type MapArgs struct {
+	Amount int
+}
+
+//MapReply xxx
+type MapReply struct {
+	File []string
+}
+
+//MapFinishArgs xxx
+type MapFinishArgs struct {
+	File string
+}
+
+//MapFinishReply xxx
+type MapFinishReply struct {
+	Done bool
+}
+
+//ReduceArgs xxx
+type ReduceArgs struct {
+}
+
+//ReduceReply xxx
+type ReduceReply struct {
+	IntermediateItems ReduceTask
+	Index             int
+}
+
+//ReduceFinishArgs xxx
+type ReduceFinishArgs struct {
+}
+
+//ReduceFinishReply xxx
+type ReduceFinishReply struct {
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the master.
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,13 +1,29 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"errors"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/rpc"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
 
+//ByKey for sorting by key.
+type ByKey []KeyValue
 
-//
-// Map functions return a slice of KeyValue.
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
+//KeyValue Map functions return a slice of KeyValue.
 //
 type KeyValue struct {
 	Key   string
@@ -24,22 +40,138 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
-// main/mrworker.go calls this function.
+//Worker main/mrworker.go calls this function.
 //
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
+	doMapTask(mapf)
+	doReduceTask(reducef)
 
 	// uncomment to send the Example RPC to the master.
 	// CallExample()
 
 }
 
-//
-// example function to show how to make an RPC call to the master.
+func doReduceTask(reducef func(string, []string) string) {
+	reduceTask, index, err := callForReduce("Master.Reduce")
+	if err != nil {
+		fmt.Println("failed to call for reduce")
+		return
+	}
+	intermediate := []KeyValue{}
+	for _, line := range reduceTask.IntermediateItems {
+		if len(line) != 0 {
+			kv := strings.Split(line, " ")
+			intermediate = append(intermediate, KeyValue{kv[0], kv[1]})
+		}
+	}
+
+	sort.Sort(ByKey(intermediate))
+
+	oname := "mr-out-" + strconv.Itoa(index)
+	ofile, _ := os.Create(oname)
+
+	//
+	// call Reduce on each distinct key in intermediate[],
+	// and print the result to mr-out-0.
+	//
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+
+		// this is the correct format for each line of Reduce output.
+		fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+
+		i = j
+	}
+
+	ofile.Close()
+	callForReduceFinish("Master.ReduceNotice")
+}
+
+func callForReduce(rpcname string) (ReduceTask, int, error) {
+	reduceArgs := ReduceArgs{}
+	reduceReply := ReduceReply{ReduceTask{make([]string, 1000)}, 0}
+
+	success := call(rpcname, &reduceArgs, &reduceReply)
+
+	if !success {
+		return ReduceTask{}, 0, errors.New("")
+	}
+
+	return reduceReply.IntermediateItems, reduceReply.Index, nil
+}
+
+func callForReduceFinish(rpcname string) {
+	reduceFinishArgs := ReduceFinishArgs{}
+	reduceFinishReply := ReduceFinishReply{}
+	call(rpcname, &reduceFinishArgs, &reduceFinishReply)
+}
+
+func doMapTask(mapf func(string, string) []KeyValue) {
+	rand.Seed(time.Now().UnixNano())
+	amount := rand.Intn(3) + 1
+	filenames, err := callForMap("Master.Map", amount)
+	fmt.Printf("File: %s\n", filenames)
+	if err != nil {
+		fmt.Println("Failed to call for map")
+		return
+	}
+
+	for _, filename := range filenames {
+		if filename != "" {
+			intermediate := []KeyValue{}
+
+			file, err := os.Open(filename)
+			if err != nil {
+				log.Fatalf("cannot open %v", filename)
+			}
+			content, err := ioutil.ReadAll(file)
+			if err != nil {
+				log.Fatalf("cannot read %v", filename)
+			}
+			file.Close()
+			kva := mapf(filename, string(content))
+			intermediate = append(intermediate, kva...)
+
+			oname := "mr-intermediate-" + filename
+			ofile, _ := os.Create(oname)
+			for _, item := range intermediate {
+				fmt.Fprintf(ofile, "%v %v\n", item.Key, item.Value)
+			}
+			ofile.Close()
+			callForMapFinish("Master.MapNotice", oname)
+		}
+	}
+}
+
+func callForMap(rpcname string, amount int) ([]string, error) {
+	mapArgs := MapArgs{amount}
+	mapReply := MapReply{make([]string, 5)}
+	if !call(rpcname, &mapArgs, &mapReply) {
+		return nil, nil
+	}
+	return mapReply.File, nil
+}
+
+func callForMapFinish(rpcname, oname string) (bool, error) {
+	mapFinishArgs := MapFinishArgs{oname}
+	mapFinishReply := MapFinishReply{}
+	call(rpcname, &mapFinishArgs, &mapFinishReply)
+	return mapFinishReply.Done, nil
+}
+
+//CallExample example function to show how to make an RPC call to the master.
 //
 // the RPC argument and reply types are defined in rpc.go.
 //
@@ -67,9 +199,9 @@ func CallExample() {
 // returns false if something goes wrong.
 //
 func call(rpcname string, args interface{}, reply interface{}) bool {
-	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
-	sockname := masterSock()
-	c, err := rpc.DialHTTP("unix", sockname)
+	c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	// sockname := masterSock()
+	// c, err := rpc.DialHTTP("unix", sockname)
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
